543e9436fbff5d8d07a20105e9877641667e0f17
ckk3 <contato.gustavomota@gmil.com>
2022-11-23 21:56:30 -0300

    create page to show payments

--- codevance/payments/urls.py
+++ codevance/payments/urls.py
@@ -1,4 +1,4 @@
-"""codevance URL Configuration
+"""payments URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('payments.urls')),
-
+    path('', views.payment_list, name='payment-list'),
 ]
--- codevance/payments/views.py
+++ codevance/payments/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Payment
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def payment_list(request):
+    payment_list = Payment.objects.all().order_by('-due_date')
+    #payment_list = Payment.objects.all().order_by('-due_date').filter(user=request.user)
+
+    #tasks_done_recently = Task.objects.filter(done='2', user=request.user,updated_at__gt=datetime.datetime.now()-datetime.timedelta(days=30)).count()
+    #tasks_done = Task.objects.filter(done='2', user=request.user).count()
+    #tasks_doing = Task.objects.filter(done='1', user=request.user).count()
+    
+    paginator = Paginator(payment_list, 3)
+
+    page = request.GET.get('page')
+
+    payments = paginator.get_page(page)
+
+    return render(request, 'payments/payments.html', {'payments': payments})
