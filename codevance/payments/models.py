882cf6808d410242ee106b93120c9cf8348906bc
ckk3 <contato.gustavomota@gmil.com>
2022-11-25 12:44:46 -0300

    add social reason and cpnj in payment model

--- codevance/payments/models.py
+++ codevance/payments/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+
 
 def get_sentinel_user():
     """
@@ -14,5 +16,9 @@ class Payment(models.Model):
     due_date = models.DateTimeField()
     original_value = models.DecimalField(max_digits=14, decimal_places=2)
     provider = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user))
-    #provider_social_reason = get_user_model().
-    #provider_cnpj = models.TextField(max_length=255)
+    provider_social_reason = models.TextField(max_length=100)
+    provider_cnpj = models.TextField(max_length=25)
+
+
+
+
