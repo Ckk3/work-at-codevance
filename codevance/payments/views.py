431f304dc6092e9524ba4e18a25d79a648e23e34
ckk3 <contato.gustavomota@gmil.com>
2022-11-26 15:30:56 -0300

    fix bug in Anticipate add

--- codevance/payments/views.py
+++ codevance/payments/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Payment
+from .models import Payment, Anticipate
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 import datetime
@@ -34,20 +34,23 @@ def anticipate_request_view(request, id):
     # Get antecipate informations
     original_value, new_value, original_due_date, new_due_date, days_delta = get_antecipate_value(payment=payment)
 
-    anticipate = Anticipate()
+    new_anticipate = Anticipate(
+        old_due_date = original_due_date,
+        new_due_date = new_due_date,
+        payment = payment,
+        original_value = original_value,
+        new_value = new_value,
+    )
 
-    if anticipate.is_valid():
-        # Create new anticipate
-        new_anticipate = anticipate.save()
-        # Add new anticipate
-        new_anticipate.save()
-        # Change payment status to requested
-        payment.status = 'requested'
-        payment.save()
+    # Add new anticipate
+    new_anticipate.save()
+    # Change payment status to requested
+    payment.status = 'requested'
+    payment.save()
 
-        #messages.info(request, 'Anticipate request done')
-        #Return to home
-        return redirect('/')
+    #messages.info(request, 'Anticipate request done')
+    #Return to home
+    return redirect('/')
 
 
 @login_required
