79db8682cd0df3d2a5e53fdc4b26ffd3483421b8
ckk3 <contato.gustavomota@gmil.com>
2022-11-26 13:43:35 -0300

    create login page, login working, login_required decorator in views and not allowing see payments from other users

--- codevance/payments/views.py
+++ codevance/payments/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Payment
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 import datetime
 from decimal import Decimal
 
-
+@login_required
 def payment_list(request):
-    """View function that return the page with user payment list"""
-    #payment_list = Payment.objects.all().order_by('-due_date').filter(provider=request.user)
-    payment_list = Payment.objects.all().order_by('-due_date', 'paid')
+    payment_list = Payment.objects.all().order_by('-due_date', 'paid').filter(provider=request.user)
     
     paginator = Paginator(payment_list, 3)
 
@@ -18,20 +17,20 @@ def payment_list(request):
 
     return render(request, 'payments/payments.html', {'payments': payments})
 
-
+@login_required
 def payment_view(request, id):
     """
     View Funcition that return payment info"""
-    #payment = get_object_or_404(Payment, pk=id, provider=request.user)
-    #get payment and require
-    payment = get_object_or_404(Payment, pk=id) 
+    payment = get_object_or_404(Payment, pk=id, provider=request.user) 
 
     original_value, new_value, original_due_date, new_due_date, days_delta = get_antecipate_value(payment=payment)
     return render(request, 'payments/payment_view.html', {'payment': payment, 'original_value': original_value, 'new_value': new_value, 'original_due_date': original_due_date, 'new_due_date': new_due_date, 'days_delta':days_delta})
 
+@login_required
 def anticipate_request_view(request, id):
     pass
 
+@login_required
 def anticipate_info_view(request, id):
     """Calculate antecipated info about a payment"""
     payment = get_object_or_404(Payment, pk=id)
@@ -39,7 +38,6 @@ def anticipate_info_view(request, id):
     if payment.paid == False:
         original_value, new_value, original_due_date, new_due_date, days_delta = get_antecipate_value(payment=payment)
         return render(request, 'payments/antecipate_payment_view.html', {'payment': payment, 'original_value': original_value, 'new_value': new_value, 'original_due_date': original_due_date, 'new_due_date': new_due_date, 'days_delta':days_delta})
-
 
 def get_antecipate_value(payment):
     """
